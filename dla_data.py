Various fixes for new location. Also make metallicity plot in log units, so it goes from -3 to -1.

dla_data.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import os.path as path
 import numpy as np
 
-def column_density_data(datadir="data", moment=False):
+def column_density_data(moment=False):
     """Plot the data on the column density function at z=3"""
+    datadir = path.dirname(__file__)
 #     celine_data(datadir)
 #     peroux_data(datadir)
     omeara_data(datadir, moment)
@@ -106,14 +107,16 @@ def prochaska_10_data(datadir="data", moment=False):
     ax.fill(10.**data[:,0],10.**cddf,'grey')
 
 
-def dndx_pro(datadir="data"):
+def dndx_pro():
     """Plot the line densities for DLAs from Prochaska & Wolfe 2009, 0811.2003"""
+    datadir = path.dirname(__file__)
     data = np.loadtxt(path.join(datadir,"dndx.txt"))
     zcen = (data[1:-1,0]+data[1:-1,1])/2.
     plt.errorbar(zcen, data[1:-1,2],xerr=[zcen-data[1:-1,0], data[1:-1,1]-zcen], yerr=data[1:-1,3], fmt="o",color="orange")
 
-def omegahi_pro(datadir="data"):
+def omegahi_pro():
     """Plot the total rho_HI density for DLAs from Prochaska & Wolfe 2009, 0811.2003"""
+    datadir = path.dirname(__file__)
     data = np.loadtxt(path.join(datadir,"dndx.txt"))
     zcen = (data[1:-1,0]+data[1:-1,1])/2.
     rhohi = data[1:-1,4]

vel_data.py
@@ -4,33 +4,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import leastsq as ls
+import os.path as path
+
+datadir = path.dirname(__file__)
 
 def load_data(zrange=None):
-    """Load the data tables and concatenate them"""
-    #data = np.loadtxt("vel_width_mtlkin_704_diff.dat")
-    #vel_data = data[:,2]
-    data2 = np.loadtxt("apj469315t2_mrt_mod.txt")
-    redshift = data2[:,0] #np.concatenate([data[:,0], data2[:,0]])
-    met = data2[:,3] #np.concatenate([data[:,1],data2[:,3]])
-    vel_data = data2[:,5] #np.concatenate([data[:,2], data2[:,5]])
+    """Load the data tables from Neeleman 2013"""
+    data2 = np.loadtxt(path.join(datadir,"apj469315t2_mrt_mod.txt"))
+    redshift = data2[:,0]
+    met = data2[:,3]
+    vel_data = data2[:,5]
     if zrange != None:
         ind = np.where((redshift < zrange[0])*(redshift > zrange[1]))
         return (redshift[ind], met[ind], vel_data[ind])
     return (redshift, met, vel_data)
 
-def pdf_with_error(vel_data, nv_table):
-    """Plot a pdf of vel_data in nv_table bins, with error bars"""
-    v_table=np.logspace(np.log10(np.min(vel_data)),np.log10(np.max(vel_data)),nv_table)
+def pdf_with_error(vel_data, v_table, lognorm=True):
+    """Plot a pdf of vel_data in v_table bins, with Poisson error bars"""
     center = np.array([(v_table[i]+v_table[i+1])/2. for i in range(0,np.size(v_table)-1)])
-
     nn = np.histogram(vel_data,v_table)[0]
     #Normalise so that integral in log space is unity.
     #This emulates np.histogram(np.log10(met), np.log10(bin),density=True)
-    width = np.array([(-np.log10(v_table[i])+np.log10(v_table[i+1])) for i in range(0,np.size(v_table)-1)])
-
-    #This is the avg. fraction of DLAs per unit absorption distance.
-    #It is needed to match the normalisation of Pontzen 2008.
-    #DLAdX = 0.065
+    if lognorm:
+        width = np.array([(-np.log10(v_table[i])+np.log10(v_table[i+1])) for i in xrange(np.size(v_table)-1)])
+    else:
+        width = np.array([(-v_table[i]+v_table[i+1]) for i in xrange(np.size(v_table)-1)])
 
     norm = width*np.size(vel_data)
 
@@ -40,29 +38,24 @@ def pdf_with_error(vel_data, nv_table):
     #These v_table will have error bars that go to 0
     ind = np.where(nn == 1)
     verr[ind] *= (0.98)
-    plt.errorbar(center,vels,xerr=[center-v_table[:-1],v_table[1:]-center],yerr=verr,fmt='.', color="black")
-
-    plt.semilogx(center, vels,'o')
     return (center, vels,verr)
 
 def plot_prochaska_2008_data(zrange = None, nv_table=7):
-    """Plot a velocity width histogram from Prochaska 2008/2013"""
+    """Plot a velocity width histogram from Neeleman 2013"""
     (_, _, vel_data) = load_data(zrange)
-    (center, vels, verr) = pdf_with_error(vel_data, nv_table)
+    v_table=np.logspace(np.log10(np.min(vel_data)),np.log10(np.max(vel_data)),nv_table)
+    #Bins should not be smaller than error on v90, which is 10.
+    if (- np.min(vel_data) + np.max(vel_data))/ nv_table < 10:
+        raise ValueError("Requested bins smaller than 10, error on v90")
+    (center, vels, verr) = pdf_with_error(vel_data, v_table)
+
+    plt.errorbar(center,vels,xerr=[center-v_table[:-1],v_table[1:]-center],yerr=verr,fmt='.', color="black")
+    plt.semilogx(center, vels,'o', color="purple")
     plt.xlim(10, 1000)
     return (center, vels,verr)
 
-def plot_alpha_metal_data(zrange=None,nv_table=7):
-    """
-       Plot the metallicities from alpha peak mainly (Si and S) elements
-       from Neeleman 2013
-       redshift  = (4,3) will show only quasars between z=4 and z=3
-    """
-    (_, met, _) = load_data(zrange)
-    return pdf_with_error(10**met, nv_table)
-
 def plot_prochaska_2008_correlation(zrange = None, color="black"):
-    """Plot the observed correlation between velocity widths and metallicity from Prochaska 2008"""
+    """Plot the observed correlation between velocity widths and metallicity from Neeleman 2013"""
     (_, met, vel_data) = load_data(zrange)
     plt.loglog(vel_data, 10**met,'o',color=color)
     vel = np.log10(vel_data)
@@ -83,17 +76,37 @@ def plot_extra_stat_hist(stat=False,zrange=None, nv_table=11):
         fmm = fmm[ind]
 
     v_table=np.linspace(0,1,nv_table)
-    center = np.array([(v_table[i]+v_table[i+1])/2. for i in range(0,np.size(v_table)-1)])
-
-    nn = np.histogram(fmm,v_table)[0]
-    #Normalise so that integral in log space is unity.
-    #This emulates np.histogram(np.log10(met), np.log10(bin),density=True)
-    width = np.array([(-v_table[i]+v_table[i+1]) for i in range(0,np.size(v_table)-1)])
-
-    norm = width*np.size(fmm)
-
-    vels = nn / norm
-    #Use poisson errors
-    verr = (np.sqrt(nn))/norm
+    (center, vels, verr) = pdf_with_error(fmm, v_table, lognorm=False)
     plt.errorbar(center,vels,xerr=[center-v_table[:-1],v_table[1:]-center],yerr=verr,fmt='.', color="black")
     plt.plot(center, vels, 'o')
+
+def load_metal_data(zrange=None):
+    """Load the data tables from Rafelski 2012"""
+    data2 = np.loadtxt(path.join(datadir,"apj433829t2_mrt.txt"), usecols=(1,11))
+    data3 = np.loadtxt(path.join(datadir,"apj433829t3_mrt.txt"),usecols=(1,11))
+    redshift = np.concatenate((data2[:,0], data3[:,0]))
+    met = np.concatenate((data2[:,1], data3[:,1]))
+    if zrange != None:
+        ind = np.where((redshift < zrange[0])*(redshift > zrange[1]))
+        redshift = redshift[ind]
+        met = met[ind]
+
+    return (redshift, met)
+
+def plot_alpha_metal_data(zrange=(3.5,2.5),nv_table=7):
+    """
+       Plot the metallicities of DLAs from the catalogues found in Rafelski 2012
+       redshift  = (4,3) will show only quasars between z=4 and z=3
+    """
+    (_, met) = load_metal_data(zrange)
+    v_table=np.linspace(np.min(met),np.max(met),nv_table)
+    #Bins should not be smaller than error on metallicity, which is about 0.2.
+    if (- np.min(met) + np.max(met))/ nv_table < 0.2:
+        raise ValueError("Requested bins smaller than 0.2, error on metallicity")
+
+    (center, vels, verr) = pdf_with_error(met, v_table, lognorm=False)
+
+    plt.errorbar(center,vels,xerr=[center-v_table[:-1],v_table[1:]-center],yerr=verr,fmt='.', color="black")
+    plt.plot(center, vels,'o', color="purple")
+
+    return (center, vels, verr)